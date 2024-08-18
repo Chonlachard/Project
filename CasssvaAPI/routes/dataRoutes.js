4b9bfa1bbf67b78265fdf2a56396c2bc81d3868e
Chonlachat Duangto <107975727+Chonlachard@users.noreply.github.com>
2024-08-18 09:53:27 +0700

    api

---
+++ CasssvaAPI/routes/dataRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// const addDataController = require('../controllers/addData');
+// const deleteDataController = require('../controllers/deleteData');
+// const getDataController = require('../controllers/getData');
+// const updateDataController = require('../controllers/updateData');
+
+// router.get('/items', getDataController.getData);
+// router.post('/items', addDataController.addData);
+// router.put('/items/:id', updateDataController.updateData);
+// router.delete('/items/:id', deleteDataController.deleteData);
+
+module.exports = router;
